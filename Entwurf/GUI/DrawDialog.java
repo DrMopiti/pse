5bfedda4f8637a463199988d09a16eb15b0ba303
RukiDV <44945464+RukiDV@users.noreply.github.com>
2018-12-16 13:47:56 +0100

    Fehler in den Klassen behoben

---
+++ Entwurf/GUI/DrawDialog.java
@@ -0,0 +1,24 @@
+package com.example.rukidev.chess;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class DrawDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button mShowDialog = (Button) findViewById(R.id.btnShowDialog);
+        mShowDialog.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
