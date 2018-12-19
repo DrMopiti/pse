c0ba4ffd861fd808d7c4ba1ab1ba7751d2170b4b
Daniel Helmig <daniel-helmig@web.de>
2018-12-19 18:29:05 +0100

    Server bzw websocket test

--- Entwurf/Server/server/src/main/java/server/ClientHandler.java
+++ Entwurf/Server/server/src/main/java/server/ClientHandler.java
@@ -1,13 +1,31 @@
 package server;
 
-import io.javalin.Javalin;
-public class ClientHandler {
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+
+import io.javalin.Javalin;
+import io.javalin.websocket.WsSession;
+
+public class ClientHandler {
+	private static Map<String, WsSession> userUsernameMap = new ConcurrentHashMap<>();
 	public static void main(String[] args) {		
 	        Javalin app = Javalin.create();
-	        //app.get("/", ctx -> ctx.result("Hello World"));	   
-	        app.ws("/", ws -> {
-	            ws.onConnect(session -> System.out.println("Connected"));
+	        app.get("/newgame", ctx -> {
+	        	ctx.result("HI");
+	        });
+	        app.get("/board/:param", ctx -> {
+	        	ctx.result(ctx.pathParam("param"));
+	        });
+	        app.get("/move/:move", ctx -> {	   
+	        	ctx.result(ctx.pathParam("move")+ userUsernameMap.toString());
+	        	broadcastMessage("player");
+	        });
+	        app.ws("/socket", ws -> {
+	        	ws.onConnect(session -> {
+	        	System.out.println("Connected");
+	        	userUsernameMap.put("player", session);
+	        	});
 	            ws.onMessage((session, message) -> {
 	                System.out.println("Received: " + message);
 	                session.getRemote().sendString("Echo: " + message);
@@ -16,7 +34,11 @@ public class ClientHandler {
 	            ws.onError((session, throwable) -> System.out.println("Errored"));
 	        });
 	        app.start(370);
-	       
+	        
 	}
-
+	
+	  private static void broadcastMessage(String user) {
+	        WsSession session = userUsernameMap.get(user);
+	        session.send("refresh faggot");	               
+	    }
 }
