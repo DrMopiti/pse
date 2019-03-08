48f9311d96a780cf4bbcd28b9826aaf7a5ce4618
Tim Groß <44930867+Timhot3p@users.noreply.github.com>
2019-03-08 19:52:05 +0100

    Neue Testklasse, mehr Abdeckung

--- Implementierung/SchachApp/app/src/test/java/com/example/user/schachapp/ChessRuleProviderTest.java
+++ Implementierung/SchachApp/app/src/test/java/com/example/user/schachapp/ChessRuleProviderTest.java
@@ -2,7 +2,9 @@ package com.example.user.schachapp;
 
 import com.example.user.schachapp.chessLogic.BoardState;
 import com.example.user.schachapp.chessLogic.ChessRuleProvider;
+import com.example.user.schachapp.chessLogic.MoveFactory;
 import com.example.user.schachapp.chessLogic.Position;
+import com.example.user.schachapp.chessLogic.Result;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -39,7 +41,7 @@ public class ChessRuleProviderTest {
     */
 
     @Test
-    public void legalMovesForE8(){
+    public void testLegalMoves(){
        BoardState testBoard = new BoardState("0B0000b0" +
                 "0LB00bl0" +
                 "0t000000" +
@@ -54,7 +56,34 @@ public class ChessRuleProviderTest {
         
     }
 
+    @Test
+    public void testStartState() {
+        BoardState testBoard = ruleProvider.getStartState();
+        assertEquals(testBoard.toString(), "TB0000btSB0000bsLB0000blDB0000bdKB0000bkLB0000blSB0000bsTB0000bt##ttttt#0");
+    }
 
+    @Test
+    public void testHasEnded() {
+        BoardState testBoard = ruleProvider.getStartState();
+        assertFalse(ruleProvider.hasEnded(testBoard));
+        testBoard.applyMove(MoveFactory.getMove("e2-e4"));
+        testBoard.applyMove(MoveFactory.getMove("e7-e5"));
+        testBoard.applyMove(MoveFactory.getMove("d1-h5"));
+        testBoard.applyMove(MoveFactory.getMove("e8-e7"));
+        testBoard.applyMove(MoveFactory.getMove("h5-e5"));
+        assertTrue(ruleProvider.hasEnded(testBoard));
+    }
 
-
+    @Test
+    public void testGetResult() {
+        BoardState testBoard = ruleProvider.getStartState();
+        assertNull(ruleProvider.getResult(testBoard));
+        testBoard.applyMove(MoveFactory.getMove("e2-e4"));
+        testBoard.applyMove(MoveFactory.getMove("e7-e5"));
+        testBoard.applyMove(MoveFactory.getMove("d1-h5"));
+        testBoard.applyMove(MoveFactory.getMove("e8-e7"));
+        testBoard.applyMove(MoveFactory.getMove("h5-e5"));
+        assertEquals(ruleProvider.getResult(testBoard).getResult(), "1:0");
+        assertEquals(ruleProvider.getResult(testBoard).getReason(), "Matt");
+    }
 }
---
+++ Implementierung/SchachApp/app/src/test/java/com/example/user/schachapp/ResultTest.java
@@ -0,0 +1,27 @@
+package com.example.user.schachapp;
+
+import com.example.user.schachapp.chessLogic.Result;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+
+public class ResultTest {
+    Result result;
+
+    @BeforeClass
+    public void setResult() {
+        result = new Result("1:0", "Matt");
+    }
+
+    @Test
+    public void testResult() {
+        assertEquals(result.getResult(), "1:0");
+    }
+
+    @Test
+    public void testReason() {
+        assertEquals(result.getReason(), "Matt");
+    }
+}
